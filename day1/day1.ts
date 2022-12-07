1bc75a9283c3d0aeaa9056225f9ab4ca523a9e94
Marco Emrich <marco.emrich@codecentric.de>
2022-12-07 01:23:16 +0100

    fixed missed function

--- day1/day1.ts
+++ day1/day1.ts
@@ -2,25 +2,33 @@ import * as R from "remeda";
 
 type Calories = Readonly<number>;
 export type Elf = Readonly<Calories[]>;
-const add = (a: number, b: number): number => a + b;
 type caloriesForElfT = (elf: Elf) => Calories;
-export const caloriesForElf: caloriesForElfT = (elf) => R.reduce(elf, add, 0);
 type weightOfHeaviestElfT = (elves: Elf[]) => Calories;
+type parseCaloriesListForElfT = (caloriesList: string) => Elf;
+type parseCaloriesListT = (list: string) => Elf[];
+type elfCarryingMostCaloriesForInputT = (list: string) => number;
+
+const add = (a: number, b: number): number => a + b;
+
+export const caloriesForElf: caloriesForElfT = (elf) => R.reduce(elf, add, 0);
+
 export const weightOfHeaviestElf: weightOfHeaviestElfT = (elves) =>
   Math.max(...R.map(elves, caloriesForElf));
-type parseCaloriesListForElfT = (caloriesList: string) => Elf;
+
 export const parseCaloriesListForElf: parseCaloriesListForElfT = (
   caloriesList
 ) => R.map(caloriesList.split("\n"), Number);
+
 const splitCaloriesListToElfCaloriesList: (caloriesList: string) => string[] = (
   caloriesList
 ) => caloriesList.split("\n\n");
-type parseCaloriesListT = (list: string) => Elf[];
-export const parseCaloriesList: parseCaloriesListT = (caloriesList) => {
-  const elves = caloriesList.split("\n\n"); /* ? */
 
-  return R.map(elves, parseCaloriesListForElf);
+export const parseCaloriesList: parseCaloriesListT = (caloriesList) => {
+  return R.map(
+    splitCaloriesListToElfCaloriesList(caloriesList),
+    parseCaloriesListForElf
+  );
 };
-type elfCarryingMostCaloriesForInputT = (list: string) => number;
+
 export const elfCarryingMostCaloriesForInput: elfCarryingMostCaloriesForInputT =
   (list) => weightOfHeaviestElf(parseCaloriesList(list));
